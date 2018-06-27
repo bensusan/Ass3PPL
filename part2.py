9b0d62d08ae1c73525659d99835d91dbde5e4bf6
Omer <hellero@post.bgu.ac.il>
2018-06-27 18:08:33 +0300

    Merge remote-tracking branch 'remotes/origin/or' into omers-branch
    # Conflicts:
    #	part2.py

--- part2.py
+++ part2.py
@@ -1,13 +1,18 @@
+import csv
+
 import numpy as np
-import random
+import sys
+import math
 
 K = 20
 L = 20
-T = 10
+T = 4
 epsilon = 0.01
+global U_counter
+U_counter = 0
 
-STList = [[1,1,3],[1,3,5],[2,3,3],[3,4,5]]
-SVList = [[2,1,2],[1,5,3],[1,3,3],[3,1,2]]
+STList = [[1,1,3],[1,3,5]]
+SVList = [[2,1,3],[1,4,3],[1,3,3],[3,1,3]]
 VList = [0,2,1,0,3]
 BList = [[2,1,1,4],[4,5,5,5],[3,3,3,5],[2,4,3,5]]
 UList = [0,3,1,2,3]
@@ -18,72 +23,94 @@ V = np.asarray(VList, dtype=np.int)
 U = np.asarray(UList, dtype=np.int)
 
 def select(n, uid, B, U, V):
+    my_data = np.loadtxt(open("ratings.csv", "rb"), usecols=(0,1),delimiter=',', skiprows=1)
+    masked = (my_data[:,0]==uid)
+    my_data = my_data[masked]
     ans=[]
-    usercluster=U[uid]
+    usercluster=U[uid-1]
     userexpectedrating=B[usercluster]
     while n>0:
         maxcluster=np.argmax(userexpectedrating)
         if userexpectedrating[maxcluster] == -1:
             break
         userexpectedrating[maxcluster] = -1
-        bestmovies=V[V==maxcluster]
-        while n>0 or len(bestmovies)>0:
-            ans.append(bestmovies[0])
-            mask = np.ones(len(bestmovies), dtype=bool)
-            mask[0] = False
-            bestmovies = bestmovies[mask]
-            n=n-1
+        reshapedV = np.reshape(V, (V.shape[0], 1))
+        id = [i for i in xrange(1, len(reshapedV) + 1)]
+        reshapedV = np.insert(reshapedV, 0, id, axis=1)
+        mask = (reshapedV[:,1]==maxcluster)
+        bestmovies = reshapedV[mask]
+        mask = np.isin(bestmovies[:,0], my_data[:,1])
+        bestmovies = bestmovies[np.logical_not(mask)]
+        while n>0 and len(bestmovies)>0:
+            ans.append(bestmovies[0][0])
+            bestmovies = np.delete(bestmovies,0,0)
+            n-=1
     return ans
 
 
 
 
 def codebook(ST,SV,K,L):
-    alliid=np.unique(ST[:,1])
-    alluid = np.unique(ST[:,0])
-    V=np.random.randint(low=0, high=L-1, size=(len(alliid),))
-    U=np.random.randint(low=0, high=K-1, size=(len(alluid),))
+    alliidST=np.unique(ST[:,1]).astype(int)
+    alluidST = np.unique(ST[:,0]).astype(int)
+    alliidSV = np.unique(SV[:,1]).astype(int)
+    alluidSV = np.unique(SV[:, 0]).astype(int)
+
+    alliid = np.unique(np.concatenate((alliidSV,alliidST),axis=0))
+    alluid = np.unique(np.concatenate((alluidSV,alluidST),axis=0))
+    V=np.random.randint(low=0, high=L, size=(max([alliid.max(),alliidSV.max()]),))
+    U=np.random.randint(low=0, high=K, size=(max([alluid.max(),alluidSV.max()]),))
+
     #V=[]
     #U=[]
     #for iid in alliid:
     #    V[iid] = random.randint(0, L-1)
     #for uid in alluid:
     #    U[uid] = random.randint(0, K - 1)
-    B = updateB(ST,V,U)
+    B = updateB(ST,V,U,K,L)
     t=1
     RSME=calculateRMSE(SV,U,V,B)
     prevRSME=0
     while not(t==T or abs(prevRSME-RSME)<=epsilon): # TODO what is terminate condition
-        nextU = []
+        print('t is now:'+str(t))
+        print('RMSE is now:' + str(RSME))
+        print('previous RMSE is now:' + str(prevRSME))
+        nextU = np.array(U)
         for uid in alluid:
-            nextU[uid] = updateU(K,ST,uid,B,V)
+            nextU[uid-1] = updateU(K,ST,uid,B,V)
         U = np.array(nextU)
-        B = updateB(ST,V,U)
-        nextV = []
+        B = updateB(ST,V,U,K,L)
+        nextV = np.array(V)
         for iid in alliid:
-            nextV[iid] = updateV(L,ST,iid,B,V)
+            nextV[iid-1] = updateV(L,ST,iid,B,V)
         V=np.array(nextV)
-        B = updateB(ST, V, U)
+        B = updateB(ST, V, U,K,L)
         prevRSME=RSME
         RSME = calculateRMSE(SV,U,V,B)
         t = t+1
-    #retrun U,V,B
+    print('exit while')
+    reshapedU = np.reshape(U, (U.shape[0], 1))
+    np.savetxt("U.csv", reshapedU, fmt='%i', delimiter=",")
 
+    reshapedV = np.reshape(V, (V.shape[0], 1))
+    np.savetxt("V.csv",reshapedV,fmt='%i',delimiter=",")
 
-
+    np.savetxt("B.csv", B, delimiter=",")
+    ans = select(10,1,B,U,V)
+    print('got ans')
 #Line 13
-def updateB(ST,V,U):
+def updateB(ST,V,U,K,L):
     #########################
-    Bans = np.ones((K,L),dtype=int)
+    Bans = np.ones((K,L),dtype=float)
     ratings = ST[:, 2]
     avagSystem = np.mean(ratings)
     Bans *= avagSystem
-    iids = ST[:, 1]
-    uids = ST[:, 0]
-    ufunc = np.vectorize(lambda uid: U[uid])
+    iids = ST[:, 1].astype(int)
+    uids = ST[:, 0].astype(int)
+    ufunc = np.vectorize(lambda uid: U[uid-1])
     uids = ufunc(uids)
 
-    vfunc = np.vectorize(lambda iid: V[iid])
+    vfunc = np.vectorize(lambda iid: V[iid-1])
     iids = vfunc(iids)
     for i in range(K):
         Iiids = (iids == i)
@@ -91,7 +118,7 @@ def updateB(ST,V,U):
             Juids=(uids == j)
             STAfterAnd=ST[np.logical_and(Iiids, Juids)]
             if not (STAfterAnd.size == 0) :
-                 Bans[i][j]=np.mean(STAfterAnd)
+                 Bans[i][j]=np.mean(STAfterAnd[:,2])
     return Bans
 
     ###########################
@@ -119,14 +146,17 @@ def updateU(K,ST,uid,B,V):
     mask = (user_ids == uid)
     masked = ST[mask]   #returns an array with ratings for specific user
     bestJ = 0;
-    bestSum = 9223372036854775807
+    bestSum = sys.maxint
     for j in range(K):
-        lst = []
-        for user_id,iid,rating in masked:
-            lst.append([(rating-B[j][V[iid-1]])**2])
+        # lst = []
+        # for user_id,iid,rating in masked:
+        #     lst.append([(rating-B[j][V[iid-1]])**2])
+        #
+        # arr = np.asarray(lst,dtype=np.int)
+        # sum = np.sum(arr)
 
-        arr = np.asarray(lst,dtype=np.int)
-        sum = np.sum(arr)
+        nowsum = np.apply_along_axis(lambda col: (col[2] - B[j][V[int(col[1]) - 1]]) ** 2, 1, masked)
+        sum = np.sum(nowsum)
         if sum<bestSum:
             bestSum = sum
             bestJ = j
@@ -146,14 +176,17 @@ def updateV(L,ST,iid,B,U):
     mask = (item_ids == iid)
     masked = ST[mask]   #returns an array with ratings for specific movie
     bestJ = 0;
-    bestSum = 9223372036854775807
+    bestSum = sys.maxint
     for j in range(L):
-        lst = []
-        for user_id,item_id,rating in masked:
-            lst.append([(rating-B[U[user_id-1]][j])**2])
+        # lst = []
+        # for user_id,item_id,rating in masked:
+        #     lst.append([(rating-B[U[user_id-1]][j])**2])
+        #
+        # arr = np.asarray(lst,dtype=np.int)
 
-        arr = np.asarray(lst,dtype=np.int)
-        sum = np.sum(arr)
+
+        nowsum = np.apply_along_axis(lambda col: (col[2] - B[U[int(col[0]) - 1]][j]) ** 2, 1, masked)
+        sum = np.sum(nowsum)
         if sum<bestSum:
             bestSum = sum
             bestJ = j
@@ -164,11 +197,22 @@ def updateV(L,ST,iid,B,U):
 
 #Line 14
 def calculateRMSE(SV,U,V,B):
-    nowsum=np.apply_along_axis(lambda col: (B[U[col[0]],V[col[1]]]-col[2])**2, 1, SV)
-    return np.sum(nowsum)
+    nowsum = np.apply_along_axis(lambda col: (B[U[int(col[0]) - 1]][V[int(col[1]) - 1]] - col[2]) ** 2, 1, SV)
+    return math.sqrt(np.mean(nowsum))
     # sum=0
     # for uid,iid,rating in SV:
     #     sum=sum+((rating-B[U[uid]][V[iid]])**2)
     # return sum
 
-codebook(ST,[],4,4)
+
+my_data = np.loadtxt("ratings.csv", delimiter=',',usecols=(0,1, 2),skiprows=95000)
+
+np.random.shuffle(my_data)
+data_length = len(my_data)
+floor = int(math.floor(data_length*0.8))
+ceil = int(math.ceil(data_length*0.8))
+ST = my_data[:floor]
+SV = my_data[ceil:]
+SV = ST
+codebook(ST,SV,20,20)
+#select(10,1,[],[],[])
